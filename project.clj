fc4685e950f142e22fca18d74471ed06892969c4
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2015-07-27 22:45:27 +0200

    Implement json generation from a folder of markdown files

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [clojure-watch "0.1.11"]]
+                 [clojure-watch "0.1.11"]
+                 [cheshire "5.5.0"]]
   :main ^:skip-aot markiki.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/markiki/core.clj
+++ src/markiki/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.tools.cli :refer [cli]]
             [clojure.string :as string]
             [clojure.java.io :as io]
-            [clojure-watch.core :refer [start-watch]])
+            [clojure-watch.core :refer [start-watch]]
+            [cheshire.core :refer :all])
   (:gen-class))
 
 (def invalid-path-error
@@ -25,12 +26,52 @@
         "Please refer to the repo for more information: https://github.com/ff-/markiki"]
        (string/join \newline)))
 
-(defn exit [status msg]
+(defn exit
+  "Exits with status leaving a message"
+  [status msg]
   (println msg)
   (System/exit status))
 
-(defn generate-wiki [path]
-  (println path))
+(defn split-title
+  "Separates the main title from a Markdown text - returns a vector [title text]"
+  [text]
+  (->> (string/split-lines text)
+       (reduce (fn [partial line]
+                 (let [title (first partial)
+                       text (second partial)]
+                   (if (and (re-find (re-pattern "^# .*") line)
+                            (empty? title))
+                     [(second (string/split line #"# ")) text]
+                     [title (str text "\n" line)])))
+               ["" ""])))
+
+(defn parse-tree
+  "Given a path scan for md files, and return a list. If folder, recur."
+  [path category]
+  (let [json (atom [])]
+    (doseq [f (.listFiles (io/file path))]
+      ;; This is ugly. Here to avoid mapping the static folder
+      (when-not (some #{(.getName f)} ["static"])
+        (swap! json
+               conj
+               (if (.isDirectory f)
+                 (let [cat-name (.getName f)
+                       [new-cat new-path] (map #(str %1 "/" cat-name) [category path])]
+                   {:category new-cat
+                    :articles (parse-tree new-path new-cat)})
+                 (let [[title content] (split-title (slurp (.getPath f)))]
+                   {:title title
+                    :lastModified (.lastModified f)
+                    :text content})))))
+  @json))
+
+(defn generate-wiki
+  "Given a OS path it will explore the folder tree and write a json in path/out/"
+  [path]
+  (when (not (.isDirectory (io/file (str path "/out"))))
+    (.mkdir (io/file (str path "/out"))))
+  (spit (str path "/out/markiki.json")
+        (generate-string (parse-tree (str path "/src") "") {:pretty true})))
 
 (defn -main [& args]
   (let [[options arguments summary] (cli args
@@ -40,17 +81,18 @@
                                          ["-w" "--watch" "Watch the folder for changes"
                                           :default false
                                           :flag true])
-        path (first arguments)]
+        path (first arguments)
+        src-path (str path "/src")]
     ;; Handle help and error conditions
     ;; The user should provide a valid directory
     (cond
      (:help options) (exit 0 (usage summary))
      (not path) (exit 1 (usage summary))
-     (not (.isDirectory (io/file path))) (exit 1 invalid-path-error))
+     (not (.isDirectory (io/file src-path))) (exit 1 invalid-path-error))
     ;; Start generating!
     (generate-wiki path)
     (when (:watch options)
-      (start-watch [{:path path
+      (start-watch [{:path src-path
                      :event-types [:create :modify :delete]
                      :bootstrap (fn [path] (println "[OK] Starting to watch " path))
                      :callback (fn [event filename]
