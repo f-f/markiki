0fd9c76848890efe206c12fc83f695a2b8e6ccff
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2015-08-04 01:02:30 +0200

    Add a list of articles

--- src/markiki/views.cljs
+++ src/markiki/views.cljs
@@ -13,6 +13,15 @@
         :on-change #(dispatch [:searchbar-change (-> % .-target .-value)])}]
       [:span.glyphicon.glyphicon-search.form-control-feedback]]]))
 
+(defn article-link []
+  (fn [{:keys [path title text last-modified]}]
+    [:li [:a {:href (str "#" path)
+              :on-click #(dispatch [:view-article path])} title]]))
+
+(defn articles-list [articles]
+  [:ul#articles-list
+   (for [article @articles]
+     [article-link article])])
 
 (defn markiki-app []
   (let [loading?      (subscribe [:loading?])
@@ -20,7 +29,16 @@
         last-article  (subscribe [:last-article])
         articles      (subscribe [:articles])]
     (fn []
-      [:div
-       [searchbar]
-       [:div "Searching for: " @searchbar-val]])))
-
+      (if @loading?
+        [:i {:class "fa fa-cog fa-spin fa-5x"
+             :style {:margin-top "3em"}}]
+        [:div
+         [searchbar]
+         (cond
+           (not (clojure.string/blank? @searchbar-val))
+           [:div "Searching " @searchbar-val]
+           (not (clojure.string/blank? @last-article))
+           [:div "last-article: " @last-article]
+           :else (if (empty? @articles)
+                   [:h2 "No articles!"]
+                   [articles-list articles]))]))))
